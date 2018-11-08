614ed082aac6bb7c9782b435ce2298dd262d553b
rajeevgUCI <rajeevg@uci.edu>
2018-11-07 22:33:32 -0800

    Add output file arg to myfunc_generator

--- src_halide_pipeline/myfunc_generator.cpp
+++ src_halide_pipeline/myfunc_generator.cpp
@@ -4,6 +4,11 @@
 using namespace Halide;
 
 int main(int argc, char **argv) {
+    if(argc != 2) {
+        std::cout << "Usage: myfunc_generator [output_file_path]" << std::endl;
+        return -1;
+    }
+
     Target target = get_target_from_environment();
     if (target.arch != Target::WebAssembly) {
         std::cout << "Skipping WebAssembly test since WebAssembly is not specified in the target.\n";
@@ -16,7 +21,7 @@ int main(int argc, char **argv) {
 
     myfunc(x, y) = x + y;
 
-    myfunc.compile_to_assembly("myfunc.s", {}, target);
+    myfunc.compile_to_assembly(argv[1], {}, target);
 
     // For reference, to view expected arguments and return type:
     // myfunc.compile_to_header("myfunc.h", {});
