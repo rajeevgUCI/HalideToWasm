cc314ba0eb3c653d099bd2757e2e2b702ece0827
rajeevgUCI <rajeevg@uci.edu>
2018-11-18 13:12:52 -0800

    Move run_demo function into halide_buffer_fns

--- src_emscripten/halide_buffer_fns.cpp
+++ src_emscripten/halide_buffer_fns.cpp
@@ -1,7 +1,28 @@
 #include "halide_buffer_fns.h"
 
+#include <iostream>
+
+#include "HalideBuffer.h"
+
 extern "C"
 {
+
+halide_buffer_t *run_demo(uint8_t *data, int width, int height)
+{
+    std::cout << "In C++" << std::endl;
+
+    // Allocates halide_buf_wrapper on heap, to prevent the buffer from being deallocated at the
+    // end of this function before the async code in halide_myfunc completes:
+    Halide::Runtime::Buffer<uint8_t> *halide_buf_wrapper = new Halide::Runtime::Buffer<uint8_t>{data, width, height};
+    halide_buffer_t *halide_buf = halide_buf_wrapper->raw_buffer();
+    std::cout << "halide_buf address = " << halide_buf << std::endl;
+
+    uint8_t *halide_data = get_halide_buffer_data(halide_buf);
+
+    return halide_buf;
+
+    // TODO: free halide_buf_wrapper. This will perhaps require a callback.
+}
 
 uint8_t *get_halide_buffer_data(halide_buffer_t *halide_buf)
 {
--- src_emscripten/halide_buffer_fns.h
+++ src_emscripten/halide_buffer_fns.h
@@ -1,11 +1,16 @@
 #ifndef HALIDE_BUFFER_FNS_H
 #define HALIDE_BUFFER_FNS_H
 
+#include <cstdint>
+
 #include "HalideBuffer.h"
+
+struct halide_buffer_t;
 
 extern "C"
 {
 
+halide_buffer_t *run_demo(uint8_t *data, int width, int height);
 uint8_t *get_halide_buffer_data(halide_buffer_t *halide_buf);
 
 }
