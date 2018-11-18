ae565b1814d7df78b2bc0796a7bc68f153f6a098
rajeevgUCI <rajeevg@uci.edu>
2018-11-17 19:11:45 -0800

    Fix bug where Halide buffer deallocated early

--- src_emscripten/run_demo.cpp
+++ src_emscripten/run_demo.cpp
@@ -14,8 +14,10 @@ void run_demo(int32_t *data, int width, int height)
 {
     std::cout << "In C++" << std::endl;
 
-    Halide::Runtime::Buffer<int32_t> halide_buf_wrapper(data, width, height);
-    halide_buffer_t *halide_buf = halide_buf_wrapper.raw_buffer();
+    // Allocates halide_buf_wrapper on heap, to prevent the buffer from being deallocated at the
+    // end of this function before the async code in halide_myfunc completes:
+    Halide::Runtime::Buffer<int32_t> *halide_buf_wrapper = new Halide::Runtime::Buffer<int32_t>{data, width, height};
+    halide_buffer_t *halide_buf = halide_buf_wrapper->raw_buffer();
     std::cout << "halide_buf address = " << halide_buf << std::endl;
 
     int32_t *halide_data = get_halide_buffer_data(halide_buf);
@@ -30,6 +32,9 @@ void run_demo(int32_t *data, int width, int height)
 
     std::cout << "Calling myfunc..." << std::endl;
     halide_myfunc(halide_buf, width, height);
+
+    // TODO: free halide_buf_wrapper after halide_myfunc completes. This will require a callback,
+    // since halide_myfunc has async code.
 }
 
 }
