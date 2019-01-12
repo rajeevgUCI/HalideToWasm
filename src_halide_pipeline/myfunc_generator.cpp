d7f001c5cc0a2941f4d8a10aa623fb10bfa8c443
rajeevgUCI <rajeevg@uci.edu>
2019-01-11 20:05:18 -0800

    Refactor myfunc_generator, remove using namespace
    Refactor myfunc_generator to remove using namespace Halide, for
    clarity/style.

--- src_halide_pipeline/myfunc_generator.cpp
+++ src_halide_pipeline/myfunc_generator.cpp
@@ -1,24 +1,22 @@
 #include "Halide.h"
 #include <iostream>
 
-using namespace Halide;
-
 int main(int argc, char **argv) {
     if(argc != 2) {
         std::cout << "Usage: myfunc_generator [output_file_path]" << std::endl;
         return -1;
     }
 
-    Target target = get_target_from_environment();
-    if (target.arch != Target::WebAssembly) {
+    Halide::Target target = Halide::get_target_from_environment();
+    if (target.arch != Halide::Target::WebAssembly) {
         std::cout << "Skipping WebAssembly test since WebAssembly is not specified in the target.\n";
         return 0;
     }
-    target.set_feature(Target::NoRuntime);
+    target.set_feature(Halide::Target::NoRuntime);
 
-    Func myfunc("myfunc");
-    Var x("x"), y("y");
-    ImageParam input(UInt(8), 2);
+    Halide::Func myfunc("myfunc");
+    Halide::Var x("x"), y("y");
+    Halide::ImageParam input(Halide::UInt(8), 2);
 
     myfunc(x, y) = 255 - input(x, y);
 
