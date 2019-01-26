00c154dba58cb3cfb25de3f70b3e0fd9524e0109
rajeevgUCI <rajeevg@uci.edu>
2019-01-26 14:42:53 -0800

    Change myfunc to apply multiple convolutions

--- src_halide_pipeline/myfunc_generator.cpp
+++ src_halide_pipeline/myfunc_generator.cpp
@@ -1,5 +1,14 @@
 #include "Halide.h"
 
+#define conv_with_suffix(i, j) \
+    RDom r_##i(filter.dim(0).min(), filter.dim(0).extent(), \
+               filter.dim(1).min(), filter.dim(1).extent(), \
+               filter.dim(2).min(), filter.dim(2).extent()); \
+    Func f_conv_##i; \
+    f_conv_##i(x, y, z, n) = bias(z); \
+    f_conv_##i(x, y, z, n) += filter(r_##i.x, r_##i.y, r_##i.z, z) \
+                              * f_conv_##j(x + r_##i.x, y + r_##i.y, r_##i.z, n);
+
 namespace {
 
 using namespace Halide;
@@ -13,21 +22,21 @@ public:
     Output<Buffer<int32_t>> output{"output", 4};
 
     void generate() {
-        /* THE ALGORITHM */
-
         Var x("x"), y("y"), z("z"), n("n");
 
         Func input_padded = BoundaryConditions::constant_exterior(input, 0);
 
-        RDom r(filter.dim(0).min(), filter.dim(0).extent(),
-               filter.dim(1).min(), filter.dim(1).extent(),
-               filter.dim(2).min(), filter.dim(2).extent());
-        Func f_conv("conv");
-        f_conv(x, y, z, n) = bias(z);
-        f_conv(x, y, z, n) += filter(r.x, r.y, r.z, z) * input_padded(x + r.x, y + r.y, r.z, n);
+        Func f_conv_0;
+        f_conv_0(x, y, z, n) = input_padded(x, y, z, n);
+
+        // Use macro to generate a pipeline of convolution operations:
+        conv_with_suffix(1, 0)
+        conv_with_suffix(2, 1)
+        conv_with_suffix(3, 2)
+        conv_with_suffix(last, 3)
 
         Func f_clamped("clamped");
-        f_clamped(x, y, z, n) = clamp(f_conv(x, y, z, n), 0, 255);
+        f_clamped(x, y, z, n) = clamp(f_conv_last(x, y, z, n), 0, 255);
 
         output(x, y, z, n) = f_clamped(x, y, z, n);
    }
